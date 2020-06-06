32af1b73ec98ed54a1ea0ff752604cd55f26bd48
testpega033 <66516296+testpega033@users.noreply.github.com>
2020-06-06 16:47:55 +0530

    done

---
+++ app.js
@@ -0,0 +1,6 @@
+//console.log({a:1,b:2});
+//console.table({a:1,b:2});
+//console.time('hello');
+//console.timeEnd('hello');
+showMessage("bhag bhag bhag dekho sher aaya sher");
+alert("abc");
---
+++ utils.js
@@ -0,0 +1,3 @@
+function showMessage(message){
+    document.getElementById("message").innerHTML = "sdsf";
+}
